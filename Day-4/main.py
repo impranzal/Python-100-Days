4e4257075c5077aeb9a91dae490385a3819b9476
Reaper <pranjalrimal.np@gmail.com>
2024-08-17 22:25:00 +0545

    Day 4

--- Day-4/main.py
+++ Day-4/main.py
@@ -1,1 +1,3 @@
-print("Hello World!")
+print("Hello World!\n\n")
+
+print("Hold fast to dreams\nFor if dreams die\nLife is a broken-winged bird\nThat cannot fly.\nHold fast to dreams\nFor when dreams go\nLife is a barren field\nFrozen with snow.")
